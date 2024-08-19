2058462a102bd384a9d443453448fea2cb980e58
dev-ravipandey1298 <154342800+dev-ravipandey1298@users.noreply.github.com>
2024-08-19 13:13:24 +0530

    Add files via upload

---
+++ notificationadminportal/src/main/java/com/hdfc/notificationadminportal/NotificationadminportalApplication.java
@@ -0,0 +1,13 @@
+package com.hdfc.notificationadminportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationadminportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationadminportalApplication.class, args);
+	}
+
+}
---
+++ notificationadminportal/src/test/java/com/hdfc/notificationadminportal/NotificationadminportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfc.notificationadminportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationadminportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
