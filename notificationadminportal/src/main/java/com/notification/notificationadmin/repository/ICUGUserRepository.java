1b1c2867ecab0b73e0cadeee08fd3c3c43710b34
Ravi Pandey <dev.ravipandey1298@gmail.com>
2024-09-11 07:50:43 +0000

    Code change

--- notificationadminportal/src/main/java/com/notification/notificationadmin/repository/ICUGUserRepository.java
+++ notificationadminportal/src/main/java/com/notification/notificationadmin/repository/ICUGUserRepository.java
@@ -1,12 +1,11 @@
 package com.notification.notificationadmin.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.notification.notificationadmin.entity.CUGUser;
 
+@Repository
 public interface ICUGUserRepository extends JpaRepository<CUGUser, String>{
 	
-	List<CUGUser> findByMobileNumberIn(List<String> mobileNumbers);
 }
